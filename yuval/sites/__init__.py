8dab1706bd3f82530fbd944c9cb912c23e36ddd2
ishai <ishai@empeeric.com>
2012-04-10 16:49:19 +0300

    locale in session

--- yuval/sites/__init__.py
+++ yuval/sites/__init__.py
@@ -1,5 +1,10 @@
 from sites.models import pageCMS
 
 def cms_processor(request):
+    locale = request.GET.get('locale')
+    if locale:
+        request.session['locale'] = locale
+    else:
+        locale = request.session.get('locale','he')	
     return { 'pages': [{'link':p['paraName'],'header':p['header']}
-    for p in pageCMS.get_pages()] ,'locale':request.GET.get('locale','he')}
+    for p in pageCMS.get_pages()] ,'locale':locale}
